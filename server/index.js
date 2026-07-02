aefc712e81fe11a74da9bcaa74bc09d199bc223c
Pradipubale <159269431+Pradipubale@users.noreply.github.com>
2026-07-02 12:17:25 +0530

    m

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-web-qiew.onrender.com",
     credentials:true
 }))
 app.use("/api/auth",authRouter)
